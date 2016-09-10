c1dd5672d4480f83053e6f647bdb8ed68b5ec923
Joshua LeDuc <joshualeduc@gmail.com>
2016-09-10 10:39:29 -0600

    added local storage system to avoid api call limit

--- info/infoSvc.js
+++ info/infoSvc.js
@@ -1,11 +1,33 @@
 angular.module('gitScan').service('infoSvc', function ($q, $http) {
 
-    this.getInfo = function (gitName) {
+    this.getInfoGithub = function (gitName) {
         var deferred = $q.defer();
         $http.get('https://api.github.com/users/' + gitName +'/repos').then(function(response) {
+            localStorage['userName'] = gitName;
+            localStorage['repos'] = JSON.stringify(response.data);
             deferred.resolve(response.data);
         });
         return deferred.promise;
-    }
-    
+    };
+
+    this.getInfoLocal = function () {
+      var deferred = $q.defer();
+      deferred.resolve(JSON.parse(localStorage['repos']));
+      return deferred.promise;
+    };
+
+    this.getInfo = function (gitName) {
+      var deferred = $q.defer();
+      if(gitName && localStorage['userName'] !== gitName){
+        this.getInfoGithub(gitName).then(function(response){
+          deferred.resolve(response);
+        });
+      } else{
+        this.getInfoLocal().then(function(response){
+          deferred.resolve(response);
+        });
+      }
+      return deferred.promise;
+    };
+
 });
--- landing/landingCtrl.js
+++ landing/landingCtrl.js
@@ -1,8 +1,8 @@
 angular.module('gitScan').controller('landingCtrl', function ($scope, landingSvc, $state) {
-    
+
     $scope.toInfo = function () {
         landingSvc.userName = $scope.gitName;
         $state.go('info');
-    }
-    
+    };
+
 });
